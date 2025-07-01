0270464c83d24f0ab3b5f97e101b74e98a418d9b
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2025-07-01 11:07:11 +0100

    Remove ingredients

--- sourcalc/core.py
+++ sourcalc/core.py
@@ -3,6 +3,13 @@ from sourcalc.classes import Ingredient, IngredientKind
 class Recipe():
     def __init__(self):
         self.ingredients = []
+        self.innoculation = 1.0
+        self.hydration = 1.0
+        self.n_loaves = 1
+        self.basket_capacity = 100
 
     def add_ingredient(self, ingredient):
         self.ingredients.append(ingredient)
+
+    def remove_ingredient(self, ingredient):
+        self.ingredients.remove(ingredient)
--- tests/recipe_tests.py
+++ tests/recipe_tests.py
@@ -4,10 +4,22 @@ from sourcalc.classes import Ingredient, IngredientKind
 class TestRecipe:
     def test_init_recipe(self):
         recipe = Recipe()
-        assert len(recipe.ingredients)==0
+        assert recipe.innoculation == 1.0
+        assert recipe.hydration == 1.0
+        assert recipe.n_loaves == 1
+        assert recipe.basket_capacity == 100
+        assert len(recipe.ingredients) == 0
     
     def test_add_ingredient(self):
         recipe = Recipe()
         ingredient = Ingredient("Wheat Flour", 1, IngredientKind.Flour)
         recipe.add_ingredient(ingredient)
-        assert len(recipe.ingredients)==1
+        assert len(recipe.ingredients) == 1
+
+    def test_remove_ingredient(self):
+        recipe = Recipe()
+        ingredient = Ingredient("Wheat Flour", 1, IngredientKind.Flour)
+        recipe.add_ingredient(ingredient)
+        assert len(recipe.ingredients) == 1
+        recipe.remove_ingredient(ingredient)
+        assert len(recipe.ingredients) == 0
